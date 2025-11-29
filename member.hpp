a2ac8eda6658803f12824cb7e5a3816eafe05fd6
Vanesa0104 <adeleonbonilla@gmail.com>
2025-11-29 14:23:59 -0500

    Update member.hpp

--- member.hpp
+++ member.hpp
@@ -5,12 +5,26 @@
 #include <string>
 using namespace std;
 
+// Header for member.cpp
+
+// Note: All times should be 24 hour format (e.g 16:00 = 4pm)
+
 class MemberSystem {
 public:
+    // Functons I will use
+
+    // Register/update members
     static void registerMember(Database& db);
+    static void updateProfile(Database& db, int memberId, const string& newName, const string& newEmail, const string& newContact);
+    
+    // Health metric and goals, basically progress
     static void addMetric(Database& db);
     static void setGoal(Database& db);
+
+    // PT sessions and class registeration
     static void schedulePT(Database& db);
+    static void registerForClass(Database& db);
+     
 };
 
 #endif
