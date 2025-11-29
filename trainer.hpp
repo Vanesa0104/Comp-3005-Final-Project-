de8639e951bbff69440abfe13ff83ec46eabffee
Vanesa0104 <adeleonbonilla@gmail.com>
2025-11-29 14:24:12 -0500

    Update trainer.hpp

--- trainer.hpp
+++ trainer.hpp
@@ -3,13 +3,31 @@
 
 #include "db.hpp"
 #include <string>
+#include <libpq-fe.h>
+#include <iostream>
+#include <map>
+#include <ctime>
+
 using namespace std;
+
+// Header for trainer.cpp
 
 class TrainerSystem {
 public:
+    // Functions i will use for trainer
+
+    // Availabilty
+    static void addWeeklyAvailability(Database& db, int trainerId);
+    static void addIndividualAvailability(Database& db, int trainerId);
+    
     static void addAvailability(Database& db);
+    static void updateAvailability(Database& db);
+
+    // Scheldule
     static void viewSchedule(Database& db);
-    static void viewMemberProgress(Database& db, int memberId);
+
+    // Members progress
+    static void viewMemberProgress(Database& db, const string& memberName);
 };
 
 #endif
